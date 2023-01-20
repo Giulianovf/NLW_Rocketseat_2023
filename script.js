c5dbbf25729f600fe4b4b387b1dffe9d28eb18c3
Giulianovf <giulianovf@hotmail.com>
2023-01-19 22:06:07 -0300

    Aula3_NWL_RocketSeat

---
+++ script.js
@@ -0,0 +1,11 @@
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+
+const data = {
+  run: ["01-01", "01-02 ", "01-06"],
+  takePills: ["01-03", "01-05","01-08"],
+  journal: ["01-04", "01-07"],
+}
+
+nlwSetup.setData(data)
+nlwSetup.load()
